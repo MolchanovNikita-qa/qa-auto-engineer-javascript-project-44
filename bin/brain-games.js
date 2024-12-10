a9ec4f5d079ce13b8522493dc3c717c30be9afbe
Molchanov Nikita <molchanov.n@finfort.ru>
2024-12-10 20:41:50 +0500

    fixed cli

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 import readlineSync from "readline-sync";
-import runGame from "..../src/cli.js";
+import runGame from "../src/cli.js";
 
 console.log("Welcome to the Brain Games!");
 runGame();
